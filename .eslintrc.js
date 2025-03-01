5264044fba1f875bde888a0a48306f3e33f03a9f
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-03-01 10:11:08 +0700

    Edit .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
   ignorePatterns: 'webpack.*.js',
+  env: {
+    browser: true,
+    es2025: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
 };
