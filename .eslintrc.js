f1ff4db8e4b4d00eff023f9b77e1e4c84b3097fc
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-05-14 23:00:20 +0700

    chore: update ESLint's ignored patterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
+/**
+ * [1]: test files are ignored because you need to specify the jsdom
+ * environment for UI tests , and you can only do this in the form of JSDoc-like
+ * comments that triggers eslint-plugin-jsdoc.
+ *
+ * Instead of turning off ESLint for all tests, it is possible to turn off the
+ * plugin's `check-tag-names` rule. But I figured that check-tag-names is more
+ * important than linting test files.
+ */
+
 module.exports = {
   extends: ['airbnb-base', 'prettier', 'plugin:jsdoc/recommended'],
-  ignorePatterns: ['webpack.*.js', 'dist'],
+  ignorePatterns: ['webpack.*.js', 'dist', '*/**/*.test.mjs'] /* [1] */,
+
   env: {
     browser: true,
     es2024: true,
