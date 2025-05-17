f2fb537751255bcb9b3f99d4abb9edc4f2f4378a
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-05-17 23:12:35 +0700

    chore: fix stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,6 @@
 /** @type {import('stylelint').Config} */
 const config = {
   extends: ['stylelint-config-sass-guidelines'],
-  ignoreFiles: ['!(./src/**/*.{css, scss, sass}'],
 };
 
 module.exports = config;
