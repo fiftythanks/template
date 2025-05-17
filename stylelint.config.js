81a81a43eaef06a67ac2e01d2101bcecb6576027
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-05-17 23:13:23 +0700

    chore: add `stylelint-config-concentric-order`

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,9 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  extends: ['stylelint-config-sass-guidelines'],
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-concentric-order',
+  ],
 };
 
 module.exports = config;
