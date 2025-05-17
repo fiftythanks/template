5665ad49092812dfdf5ffa587b19de39c3cb5116
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-05-17 23:25:49 +0700

    chore: turn off `@stylistic/function-parentheses-space-inside`

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,10 @@ const config = {
     'stylelint-config-sass-guidelines',
     'stylelint-config-concentric-order',
   ],
+  rules: {
+    /* This rule conflicts with Prettier */
+    '@stylistic/function-parentheses-space-inside': null,
+  },
 };
 
 module.exports = config;
